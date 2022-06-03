a45c7d04bbe97081da15f27941e6121c2c6c5344
Beasley <beasley.com@gmail.com>
2022-06-03 13:47:03 -0700

    got basic design structure finished

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Navbar';
+import TextBox from './TextBox';
+
+export default function Header() {
+  return (
+    <>
+    <TextBox text="Beasley"/>
+    <Navbar/>
+    </>
+  )
+}
